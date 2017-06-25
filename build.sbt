61d7bb015e95e70d17978747643c9c7849224f2e
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2017-06-25 21:05:42 +0300

    Add web server implementation

--- build.sbt
+++ build.sbt
@@ -4,9 +4,10 @@ lazy val root = (project in file("."))
     scalaVersion := "2.12.2",
     version      := "0.1.0-SNAPSHOT",
     libraryDependencies ++= Seq(
-      "com.typesafe.akka" %% "akka-actor" % "2.5.3",
-      "com.typesafe.akka" %% "akka-testkit" % "2.5.3" % Test,
+      "com.typesafe.akka" %% "akka-actor" % "2.4.19",
+      "com.typesafe.akka" %% "akka-testkit" % "2.4.19" % Test,
       "com.typesafe.akka" %% "akka-http" % "10.0.8",
+      "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.8",
 
       "org.scalactic" %% "scalactic" % "3.0.1",
       "org.scalatest" %% "scalatest" % "3.0.1" % Test,
---
+++ src/main/scala/googlesearch/WebServer.scala
@@ -0,0 +1,73 @@
+package googlesearch
+
+import java.util.UUID
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives.{path, _}
+import akka.pattern.ask
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import googlesearch.actors.{GoogleRequestActor, SearchHistoryActor}
+import spray.json.DefaultJsonProtocol._
+
+import scala.concurrent.duration._
+import scala.io.StdIn
+import scala.util.{Failure, Success}
+
+object WebServer {
+
+  case class SearchResult(urls: Seq[String])
+  case class SearchHistory(queries: Seq[String])
+
+  implicit val searchResultFormat = jsonFormat1(SearchResult)
+  implicit val searchHistoryFormat = jsonFormat1(SearchHistory)
+
+  def main(args: Array[String]) {
+    implicit val system = ActorSystem()
+    implicit val timeout: Timeout = 5.seconds
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val http = Http(system)
+
+    // TODO: it is better to have custom supervisor of these two actors
+    val historyActor = system.actorOf(SearchHistoryActor.props, "history")
+    val requestActor = system.actorOf(GoogleRequestActor.props(
+      historyActor, request => http.singleRequest(request)))
+
+    val route =
+      pathPrefix("search") {
+        (get & path(Segment)) { (query) =>
+          val requestId = UUID.randomUUID().toString
+
+          onComplete(requestActor ? GoogleRequestActor.SearchFor(requestId, query)) {
+            case Success(GoogleRequestActor.SearchResults(`requestId`, urls)) =>
+              complete(SearchResult(urls))
+            case Success(GoogleRequestActor.QueryLimitExceeded) =>
+              complete(StatusCodes.Forbidden)
+            case Success(GoogleRequestActor.SearchError(code)) =>
+              complete(code)
+            case Failure(e) => failWith(e)
+          }
+        }
+      } ~ path("history") {
+        get {
+          val requestId = UUID.randomUUID().toString
+
+          complete((historyActor ? SearchHistoryActor.Remind(requestId)) map {
+            case SearchHistoryActor.Remembered(`requestId`, queries) => SearchHistory(queries.seq)
+          })
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+
+    StdIn.readLine()
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+  }
+}
